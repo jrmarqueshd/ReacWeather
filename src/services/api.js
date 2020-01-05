6325c33dde529ed55e0b488d2bb72351b583c6e7
jrmarqueshd <jrmarqueshd@gmail.com>
2020-01-05 16:43:46 -0300

    fix: change api for complete data

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 dotenv.config();
 
 const api = axios.create({
-	baseURL: `https://api.hgbrasil.com/weather?format=json-cors&key=${process.env.REACT_APP_API_WEATHER_KEY}`
+	baseURL: `https://api.hgbrasil.com/weather?format=json-cors&key=33f71e3b&woeid=455857`
 });
 
 export default api;
